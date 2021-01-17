185d82285ae82cac6c269da6afeaca95bf693414
Sergey Galich <9985613@gmail.com>
2021-01-17 15:00:06 -0800

    first commit

---
+++ train.py
@@ -0,0 +1,14 @@
+def jumpSearch(arr, S):
+	arr.sort()    # Jump search is for sorted arrays only
+	step = int(len(arr)**.5)
+	end = step
+	while end <= len(arr):
+		if arr[end - 1] >= S:
+			# If number S should be in this interval =>
+			# do a linear search
+			for i in range(end - step, end):
+				if arr[i] == S:
+					return True
+		else:    # Jump to another interval
+			end += step
+	return False
